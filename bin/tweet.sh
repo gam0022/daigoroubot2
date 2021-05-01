83f7d6307f2e9833619c344a8fc4b029efc3c8da
gam0022 <gam0022@gmail.com>
2021-05-01 14:03:00 +0000

    work tweet.rb

--- bin/tweet.sh
+++ bin/tweet.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd $HOME/daigoroubot2
-MECAB_PATH=/usr/lib/libmecab.so.2 /usr/bin/ruby2.3.3 bin/tweet.rb $* 1>> logs/tweet.log 2>> logs/tweet.log
+MECAB_PATH=/usr/lib/libmecab.so.2 /usr/bin/ruby bin/tweet.rb $* 1>> logs/tweet.log 2>> logs/tweet.log
